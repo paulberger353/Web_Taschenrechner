dea826cb99c4ab93c86cad166c0fff7baa16b5f0
Berg <paul.berg@mk-versuchsanlagen.de>
2025-10-09 10:46:27 +0200

    GrundGUI erstellt, keine Funktionalitaet

--- Web_Taschenrechner/Default.aspx
+++ Web_Taschenrechner/Default.aspx
@@ -5,15 +5,58 @@
 <head>
     <link href="Style/Taschenrechner.css" rel="stylesheet" type="text/css" />
     <style type="text/css">
-        .display {}
+        form{
+            margin: 10% 20%;
+            background-color: gray;
+        }
+
+        .panDisplay, .row {
+
+        }
     </style>
 </head>
 <body>
     <form runat="server">
-        <asp:TextBox ID="txtDisplay" CssClass="display" runat="server"></asp:TextBox>
+        <div class="panDisplay" >
+            <asp:TextBox ID="txtDisplay" CssClass="display" runat="server" />
+            <asp:Label ID="labRechenzeichen" CssClass="label" runat="server" Text="hallo" />
+        </div>
 
-        <div class="buttons">
-            <asp:Button ID="btn1" Text="1" CssClass="btn" runat="server"/>
+        <div class="allButtons">
+            <div class="panNumbs">
+                <asp:Button ID="btnClear" Text="C" CssClass="btnlong operator" runat="server"/>
+            
+                <div class="row">                
+                    <asp:Button ID="btn1" Text="1" CssClass="btn numb" runat="server"/>
+                    <asp:Button ID="btn2" Text="2" CssClass="btn numb" runat="server"/>
+                    <asp:Button ID="btn3" Text="3" CssClass="btn numb" runat="server"/>
+                </div>
+            
+                <div class="row">                
+                    <asp:Button ID="Button4" Text="4" CssClass="btn numb" runat="server"/>
+                    <asp:Button ID="Button5" Text="5" CssClass="btn numb" runat="server"/>
+                    <asp:Button ID="Button6" Text="6" CssClass="btn numb" runat="server"/>
+                </div>
+            
+                <div class="row">                
+                    <asp:Button ID="Button7" Text="7" CssClass="btn numb" runat="server"/>
+                    <asp:Button ID="Button8" Text="8" CssClass="btn numb" runat="server"/>
+                    <asp:Button ID="Button9" Text="9" CssClass="btn numb" runat="server"/>
+                </div>
+            
+                <div class="row" >
+                    <asp:Button ID="btnKomma" Text="0" CssClass="btn operator" runat="server"/>
+                    <asp:Button ID="btn0" Text="0" CssClass="btn numb" runat="server"/>
+                    <asp:Button ID="btnGleich" Text="0" CssClass="btn operator" runat="server"/>
+                </div>
+            </div>
+
+            <div class="panOperatoren" >
+                <asp:Button ID="btnPlus" Text="+" CssClass="btn operator" runat="server" />
+                <asp:Button ID="btnMinus" Text="-" CssClass="btn operator" runat="server"/>
+                <asp:Button ID="btnMal" Text="*" CssClass="btn operator" runat="server"/>
+                <asp:Button ID="btnGeteilt" Text="÷" CssClass="btn operator" runat="server"/>
+            </div>
         </div>
     </form>
 </body>
--- Web_Taschenrechner/Default.aspx.designer.cs
+++ Web_Taschenrechner/Default.aspx.designer.cs
@@ -24,6 +24,24 @@ namespace Web_Taschenrechner
         protected global::System.Web.UI.WebControls.TextBox txtDisplay;
 
         /// <summary>
+        /// labRechenzeichen-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label labRechenzeichen;
+
+        /// <summary>
+        /// btnClear-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnClear;
+
+        /// <summary>
         /// btn1-Steuerelement
         /// </summary>
         /// <remarks>
@@ -31,5 +49,140 @@ namespace Web_Taschenrechner
         /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btn1;
+
+        /// <summary>
+        /// btn2-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn2;
+
+        /// <summary>
+        /// btn3-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn3;
+
+        /// <summary>
+        /// Button4-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button4;
+
+        /// <summary>
+        /// Button5-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button5;
+
+        /// <summary>
+        /// Button6-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button6;
+
+        /// <summary>
+        /// Button7-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button7;
+
+        /// <summary>
+        /// Button8-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button8;
+
+        /// <summary>
+        /// Button9-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button9;
+
+        /// <summary>
+        /// btnKomma-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnKomma;
+
+        /// <summary>
+        /// btn0-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btn0;
+
+        /// <summary>
+        /// btnGleich-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnGleich;
+
+        /// <summary>
+        /// btnPlus-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnPlus;
+
+        /// <summary>
+        /// btnMinus-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnMinus;
+
+        /// <summary>
+        /// btnMal-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnMal;
+
+        /// <summary>
+        /// btnGeteilt-Steuerelement
+        /// </summary>
+        /// <remarks>
+        /// Automatisch generiertes Feld
+        /// Zum Ändern Felddeklaration aus der Designerdatei in eine Code-Behind-Datei verschieben.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnGeteilt;
     }
 }
